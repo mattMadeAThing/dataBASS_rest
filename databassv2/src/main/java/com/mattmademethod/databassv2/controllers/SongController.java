676fbdee447cd991b8a3a071e1c75b025456dd98
Matt Messer <51376954+mattMadeAThing@users.noreply.github.com>
2019-08-19 12:38:38 -0500

    Add files via upload

--- databassv2/src/main/java/com/mattmademethod/databassv2/controllers/SongController.java
+++ databassv2/src/main/java/com/mattmademethod/databassv2/controllers/SongController.java
@@ -2,6 +2,7 @@ package com.mattmademethod.databassv2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,22 +28,24 @@ public class SongController {
 	private SongRepository songRepository;
 	
 	
-	
+	@CrossOrigin
 	@GetMapping("/byartist/{artist}")
 	public @ResponseBody Iterable<Song> getSongsByArtist(@RequestParam String artistInput){
 		return songRepository.findByArtist(artistInput);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/bysong/{songName}") 
 	public @ResponseBody Iterable<Song> getSongsByTitle(@RequestParam String songName) {
 		return songRepository.findBySongName(songName);
 	}
-	
+	@CrossOrigin
 	@GetMapping("/activereqs") 
 	public @ResponseBody Iterable<Song> getActiveReqs() {
 		return songRepository.activeReqs();
 	}
 	
+	@CrossOrigin
 	@PatchMapping("/flipactive/{id}")
 	public void flipActive(@RequestParam int id){
 		Song flip = songRepository.findById(id).get();
@@ -50,12 +53,14 @@ public class SongController {
 		songRepository.save(flip);
 	}
 	
+	@CrossOrigin
 	@PatchMapping("rateup/{id}")
 	public void rateUp(@RequestParam int id){
 		Song song = songRepository.findById(id).get();
 		song.setRating(song.getRating() + 1);
 		songRepository.save(song);
 	}
+	@CrossOrigin
 	@PatchMapping("ratedown/{id}")
 	public void rateDown(@RequestParam int id) {
 		Song song = songRepository.findById(id).get();
